79c352934fbb6415f9f50c29e9581f584616c595
oziel <oziel.trevisan@gmailcom>
2021-02-25 13:37:44 -0300

    criado atributo de classe

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,5 @@
 class Pessoa:
+    olhos = 2
     def __init__(self, *filhos,  nome=None, idade=35):
         self.idade = idade
         self.nome = nome
@@ -21,8 +22,15 @@ if __name__ == '__main__':
         print(filho.nome)
     luciano.sbrenome = 'ramalho'
     del luciano.filhos
+    luciano.olhos = 1
+    oziel.olhos = 4
+    del oziel.olhos
     print(luciano.__dict__)
     print(oziel.__dict__)
+    print(luciano.olhos)
+    print(oziel.olhos)
+    print(id(pessoa .olhos), id(oziel.olhos), id(luciano .olhos))
+
 
 
 
