38b9576e5fb4a537b1b365481756a486fd91ddad
oziel <oziel.trevisan@gmailcom>
2021-02-25 10:41:10 -0300

    criado repositrio atributo filho

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,21 +1,25 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, *filhos,  nome=None, idade=35):
         self.idade = idade
         self.nome = nome
+        self.filhos = list (filhos)
+
 
     def cumprimentar (self):
         return f'Olá {id (self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('luciano')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'oziel'
-    print(p.nome)
-    print(p.idade)
+    oziel = Pessoa(nome='oziel')
+    luciano = Pessoa (oziel, nome='luciano')
+    print(Pessoa.cumprimentar(luciano))
+    print(id(luciano))
+    print(luciano.cumprimentar())
+    print(luciano.nome)
+    print(luciano.idade)
+    for filho in luciano.filhos:
+        print(filho.nome)
+
 
 
 
