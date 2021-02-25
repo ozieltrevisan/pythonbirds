e276c32c63f53a81c1fe2470a22d06a49f485022
oziel <oziel.trevisan@gmailcom>
2021-02-25 09:48:50 -0300

    criado metado compimentar

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,12 @@
 class Pessoa:
-    pass
+    def comprimentar (self):
+        return f'Olá {id (self)}'
+
+
+if __name__ == '__main__':
+    p = Pessoa()
+    print(Pessoa.comprimentar(p))
+    print(id(p))
+    print(p.comprimentar())
+
+
